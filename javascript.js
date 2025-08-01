8cd11d1db7decd1754571cf5d41a7bb0a95febf3
jerryluo <jerryluogd@gmail.com>
2025-07-31 20:57:31 -0400

    Made code simpler, Added random color, Changed border opacity

--- javascript.js
+++ javascript.js
@@ -1,34 +1,32 @@
 const mainDiv = document.querySelector(".main-div");
-function grid() {
-    for (let i = 0; i < 16 ** 2; i++) {
+const btn = document.querySelector("button");
+
+function getRGB() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function square(input = 16) {            
+    for (let i = 0; i < input ** 2; i++) {
+        let color = getRGB();
         let miniDiv = document.createElement("div");
         miniDiv.classList.add("grid-square");
-        mainDiv.appendChild(miniDiv);
-        miniDiv.style.width = "40px";
+        miniDiv.style.width = `${640 / input}px`;
         miniDiv.style.height = miniDiv.style.width;
-        miniDiv.addEventListener("mouseover", () => miniDiv.style.backgroundColor = "blue");
+        mainDiv.appendChild(miniDiv);
+        miniDiv.addEventListener("mouseover", () => miniDiv.style.backgroundColor = `${color}`);        
     }
 }
 
-function square() {
-    const btn = document.querySelector("button");
-    btn.addEventListener("click", () => {
-        let input = prompt("Squares per Side");
-        if (input > 100) {
-            input = prompt("Maximum 100");
-        }
-        const divs = document.querySelectorAll(".grid-square")
-        divs.forEach(sq => sq.remove());
-        for (let i = 0; i < input ** 2; i++) {
-            let miniDiv = document.createElement("div");
-            miniDiv.classList.add("grid-square");
-            miniDiv.style.width = `${640 / input}px`;
-            miniDiv.style.height = miniDiv.style.width;
-            mainDiv.appendChild(miniDiv);
-            miniDiv.addEventListener("mouseover", () => miniDiv.style.backgroundColor = "blue");
-        }
-    });
-}
+btn.addEventListener("click", () => {
+    let input = prompt("Squares per Side (max 100)");
+    if (input > 100) {input = 100}
+    if (input < 1 || isNaN(input)) {input = 16}
+    const divs = document.querySelectorAll(".grid-square")
+    divs.forEach(sq => sq.remove());
+    square(Number(input));
+});
 
-grid();
 square();
